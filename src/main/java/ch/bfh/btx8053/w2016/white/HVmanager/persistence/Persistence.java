644015201813046496eee689ba7d0c7b28c4f9aa
Caldf1 <fabiennenicole.calderara@students.bfh.ch>
2016-12-21 15:47:55 +0100

    leere Klassen zum Zweiten

---
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/persistence/Persistence.java
@@ -0,0 +1,5 @@
+package ch.bfh.btx8053.w2016.white.HVmanager.persistence;
+
+public class Persistence {
+
+}
---
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/test/Tests.java
@@ -0,0 +1,5 @@
+package ch.bfh.btx8053.w2016.white.HVmanager.test;
+
+public class Tests {
+
+}
