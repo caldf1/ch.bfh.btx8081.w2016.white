b4662cb7c67cfc3be2475f40f09822c130116bf3
Caldf1 <fabiennenicole.calderara@students.bfh.ch>
2016-12-23 22:50:17 +0100

    wieder entkommentiert --> library herunterladen: www.eclipse.org/eclipselink/#download

--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/persistence/DatabaseConnector.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/persistence/DatabaseConnector.java
@@ -1,53 +1,53 @@
 package ch.bfh.btx8053.w2016.white.HVmanager.persistence;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//
-//import ch.bfh.btx8053.w2016.white.HVmanager.model.Person;
-//import ch.bfh.btx8053.w2016.white.HVmanager.model.PersonType;
-//
-///**
-// * 
-// * @author nedot1
-// *
-// */
-//
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import ch.bfh.btx8053.w2016.white.HVmanager.model.Person;
+import ch.bfh.btx8053.w2016.white.HVmanager.model.PersonType;
+
+/**
+ * 
+ * @author nedot1
+ *
+ */
+
 public class DatabaseConnector {
-//	
-//	private static final String PERSISTENCE_UNIT_NAME = "HVManager";
-//	EntityManager em;
-//	EntityManagerFactory factory;
-//	EntityTransaction transaction;
-//	
-//	public DatabaseConnector() {
-//		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-//						
-//	}
-//	
-//	public void insertPerson(Person person){
-//		EntityManager em = factory.createEntityManager();
-//		EntityTransaction transaction = em.getTransaction();
-//		transaction.begin();
-//		em.persist(person);
-//		em.getTransaction().commit();
-//		em.close();
-//	}
-//	
-//	public void update(Person person){
-//		EntityManager em = factory.createEntityManager();
-//		EntityTransaction transaction = em.getTransaction();
-//		transaction.begin();
-//		em.merge(person);
-//		em.getTransaction().commit();
-//		em.close();
-//	}
-//	
-//	public void deletePerson(Person person){
-//		EntityTransaction transaction = em.getTransaction();
-//		em.remove(person);
-//		em.getTransaction().commit();
-//		em.close();
-//	}
+	
+	private static final String PERSISTENCE_UNIT_NAME = "HVManager";
+	EntityManager em;
+	EntityManagerFactory factory;
+	EntityTransaction transaction;
+	
+	public DatabaseConnector() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+						
+	}
+	
+	public void insertPerson(Person person){
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(person);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public void update(Person person){
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.merge(person);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public void deletePerson(Person person){
+		EntityTransaction transaction = em.getTransaction();
+		em.remove(person);
+		em.getTransaction().commit();
+		em.close();
+	}
 }
