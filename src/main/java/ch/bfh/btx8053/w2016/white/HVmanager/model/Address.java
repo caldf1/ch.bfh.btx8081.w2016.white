87dfe8ac2ab872cc11e12e4b9fde502c92ae2407
umern1 <uasne@LEMBE0E2W.e-mediat.net>
2016-12-28 20:00:33 +0100

    Anpassung Klasse Address

--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/model/Address.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/model/Address.java
@@ -1,66 +1,135 @@
 package ch.bfh.btx8053.w2016.white.HVmanager.model;
 
 /**
- * @author umern11
+ * @author umern11, caldf1
  *
  */
-
-//test
-//test
-
 public class Address {
 	 
-    private String street;
-    private int zip;
-    private String city;
-    private AddressType addressType;
-    private int personId;
-    private int externalInstitutionId;
+/*==============================================
+ *    Attributes
+ *==============================================
+ */ 
+    
+	private AddressType addressType = null;
+	private String street = null;
+    private String zip = null;
+    private String city = null;
+    private String country = "CH";
+    private String postofficebox = null;
+    
+    private String phonenumber = null;
+    private String mobilenumber = null;
+    private String faxnumber = null;
+    private String email = null;
+    
+    // private Date creationDate = null;
+    // private Date gueltigAb = null;
+    // private Date gueltigBis = null;
+    
+   
+
+    
+    
+/*==============================================
+ *    Constructor
+ *==============================================
+ */   
      
-    public Address(int Id, String street, int zip, String city, AddressType addressType, String type) {
-        if (type.equals("p")){
-            this.personId = Id;
-        }
-        else {
-            this.externalInstitutionId = Id;
-        }
+    public Address(String street, String zip, String city, AddressType addressType) {
+      
         this.street = street;
         this.zip = zip;
         this.city = city;
         this.addressType = addressType;
+        
+    
+      
+        
     }
  
+    
+/*==============================================
+ *    GETTER
+ *==============================================
+ */
+    
     public String getStreet() {
         return street;
     }
  
-    public void setStreet(String street) {
-        this.street = street;
-    }
- 
-    public int getZip() {
+    
+    public String getPLZ() {
         return zip;
     }
- 
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
+     
  
     public String getCity() {
         return city;
     }
  
+    public String getPostOfficeBox(){
+    	return postofficebox;
+    }
+    
+    public AddressType getAddressType(){
+    	return addressType;
+    }
+   
+ 
+	@Override
+	public String toString() {
+		return "Address: " + street + ", " + zip + " " + city + ", " + country + "/nPhone: " + phonenumber + "/nMobile: " + mobilenumber + "/nFax: " + faxnumber + "/nEmail: " + email;
+	}
+	
+	
+
+/*==============================================
+ *    SETTER
+ *==============================================
+ */ 
+    
+    public void addPhonenumber(String phonenumber){
+    	this.phonenumber = phonenumber;
+    }
+
+    public void addMobilenumber(String mobilenumber){
+    	this.mobilenumber = mobilenumber;
+    }
+    
+    public void addFaxnumber(String faxnumber){
+    	this.faxnumber = faxnumber;
+    }
+    
+    public void addEmail(String email){
+    	this.email = email;
+    }
+    
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
     public void setCity(String city) {
         this.city = city;
     }
- 
-    public AddressType getAdressType() {
-        return addressType;
+    
+   public void setCountry(String country){
+	   this.country = country;
+   }
+    
+   public void setPostfach(String postofficebox){
+	   this.postofficebox = postofficebox;
+   }
+   
+   
+    public void setAddress(String street, String zip, String city){
+    	this.street = street;
+    	this.zip = zip;
+    	this.city = city;
     }
- 
-    public void setAddressType(AddressType addressType) {
-        this.addressType = addressType;
-    }
-     
-     
+    
 }
