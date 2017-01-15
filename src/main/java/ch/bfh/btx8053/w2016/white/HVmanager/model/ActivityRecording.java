12a2087815547c5bc109c8ff13da1cfaa81bf8c8
umern11 <uasne@LEMBE0E2W.e-mediat.net>
2017-01-15 22:40:00 +0100

    Neue Klasse Activity Recording

--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/model/ActivityRecording.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/model/ActivityRecording.java
@@ -1,9 +1,127 @@
 package ch.bfh.btx8053.w2016.white.HVmanager.model;
-
+ 
+ 
 /**
- * not implemented
- * @author: team white
+ * 
+ * @author: umern11, caldf1
  */
 public class ActivityRecording {
-
+     
+     
+    /*==============================================
+     *    Attributes
+     *==============================================
+     */
+     
+     
+    private String date = null;
+    private String description = null;
+    private String quantity = null;
+    
+ 
+     
+    /*==============================================
+     *    Constructor
+     *==============================================
+     */
+     
+    /**
+     * 
+     */
+    public ActivityRecording(){
+         
+    }
+     
+    /**
+     * 
+     * @param date
+     * @param description
+     * @param quantity
+     */
+    public ActivityRecording (String date, String description, String quantity) {
+         
+        this.date = date;
+        this.description = description;
+        this.quantity = quantity;
+               
+    }
+ 
+ 
+     
+    /*==============================================
+     *    GETTER
+     *==============================================
+     */
+     
+    /**
+     * 
+     * @return
+     */
+    public String getDate() {
+        return date;
+    }
+ 
+ 
+    /**
+     * 
+     * @return
+     */
+    public String getDescription() {
+        return description;
+    }
+ 
+ 
+ 
+    /**
+     * 
+     * @return
+     */
+    public String getQuantity() {
+        return quantity;
+    }
+ 
+ 
+    /**
+     * 
+     */
+    @Override
+    public String toString(){
+        return "\nActivityRecording: \nDatum: " + date + "\nBeschreibung: " + description + "\nQuantität: " + quantity;
+    }
+ 
+ 
+/*==============================================
+ *    SETTER
+ *==============================================
+ */
+    
+ 
+    /**
+     * 
+     * @param date
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+ 
+ 
+    /**
+     * 
+     * @param description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+ 
+ 
+ 
+    /**
+     * 
+     * @param quantity
+     */
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+    
+     
 }
