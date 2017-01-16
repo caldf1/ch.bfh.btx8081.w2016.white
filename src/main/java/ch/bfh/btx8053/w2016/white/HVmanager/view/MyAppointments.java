e72ce9e38b67570699fe6aa2b245e9ce879dede8
heldf1 <florian.held@students.bfh.ch>
2017-01-16 03:11:36 +0100

    Changes views MyAppointments and Substitute
    Changed all.

--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/MyAppointments.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/MyAppointments.java
@@ -1,4 +1,4 @@
-package ch.bfh.btx8053.w2016.white.HVmanager.view;
+package HomeScreen;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -15,6 +15,17 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class MyAppointments extends VerticalLayout implements View {
 
+	private Grid myAppointments;
+	private Button homeBtn;
+	private Button addBtn;
+	private Button cancelBtn;
+	private Button editBtn;
+	private HorizontalLayout horizontal;
+
+	///////// VIEW SIZE /////////
+	final static String WIDTH = "280";
+	final static String HEIGHT = "570";
+	////////////////////////////////////
 
 	@Override
 	public void enter(ViewChangeEvent event) {
@@ -22,14 +33,49 @@ public class MyAppointments extends VerticalLayout implements View {
 	}
 
 	/**
-	 * 
+	 * This constructor shows one image "Site under construction" that the view
+	 * has some content.
 	 * 
 	 * @param myui
 	 */
 	public MyAppointments(MyUI myui) {
 	
 
+		///// Billing Overview /////
+		this.myAppointments = new Grid("Meine Termine:");
+		myAppointments.setWidth(WIDTH);
+		myAppointments.setHeight(HEIGHT);
+
+		myAppointments.addColumn("Datum");
+		myAppointments.addColumn("Zeit");
+		myAppointments.addColumn("cID");
+		myAppointments.addColumn("Klienten Vorname");
+		myAppointments.addColumn("Klienten Nachname");
+		myAppointments.addRow("15.01.2017", "15:00", "10079", "Hans", "Muster");
+		myAppointments.addRow("17.01.2017", "09:00", "10080", "Beatrice", "Müller");
+		//////////////////////////////////////////////////////
+
+		///// Home Button /////
+		this.horizontal = new HorizontalLayout();
+		this.homeBtn = new Button(FontAwesome.HOME);
+		this.addBtn = new Button(FontAwesome.PLUS);
+		this.cancelBtn =new Button(FontAwesome.MINUS);
+		this.editBtn = new Button(FontAwesome.EDIT);
+
+		homeBtn.addClickListener(e -> {
+			myui.getNavigator().navigateTo(myui.HOMESCREEN);
+		});
+
+		horizontal.addComponents(homeBtn, addBtn, cancelBtn, editBtn);
+		horizontal.setSpacing(true);
+		//////////////////////////////////////////////////////
+
+		///// Root /////
+		this.addComponents(myAppointments, horizontal);
+		this.setMargin(true);
+		this.setSpacing(true);
 
 	}
 
 }
+
--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/Substitute.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/Substitute.java
@@ -1,4 +1,4 @@
-package ch.bfh.btx8053.w2016.white.HVmanager.view;
+package HomeScreen;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -15,18 +15,65 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class Substitute extends VerticalLayout implements View {
 
+	private Grid substitute;
+	private Button homeBtn;
+	private Button addBtn;
+	private Button cancelBtn;
+	private Button editBtn;
+	private HorizontalLayout horizontal;
+
+	///////// VIEW SIZE /////////
+	final static String WIDTH = "280";
+	final static String HEIGHT = "570";
+	////////////////////////////////////
+
 	@Override
 	public void enter(ViewChangeEvent event) {
 
 	}
 
 	/**
-	 *
+	 * This constructor shows one image "Site under construction" that the view
+	 * has some content.
 	 * 
 	 * @param myui
 	 */
 	public Substitute(MyUI myui) {
 	
+
+		///// Billing Overview /////
+		this.substitute = new Grid("Stellvertretungen:");
+		substitute.setWidth(WIDTH);
+		substitute.setHeight(HEIGHT);
+
+		substitute.addColumn("Von:");
+		substitute.addColumn("Bis:");
+		substitute.addColumn("Pfleger ID");
+		substitute.addColumn("Pfleger Vorname: ");
+		substitute.addColumn("Pfleger Nachname:");
+		substitute.addRow("15.01.2017", "20.01.2017", "2079", "Beat", "Müller");
+		substitute.addRow("22.01.2017", "25.01.2017", "2080", "Leony", "Kuster");
+		//////////////////////////////////////////////////////
+
+		///// Home Button /////
+		this.horizontal = new HorizontalLayout();
+		this.homeBtn = new Button(FontAwesome.HOME);
+		this.addBtn = new Button(FontAwesome.PLUS);
+		this.cancelBtn =new Button(FontAwesome.MINUS);
+		this.editBtn = new Button(FontAwesome.EDIT);
+
+		homeBtn.addClickListener(e -> {
+			myui.getNavigator().navigateTo(myui.HOMESCREEN);
+		});
+
+		horizontal.addComponents(homeBtn, addBtn, cancelBtn, editBtn);
+		horizontal.setSpacing(true);
+		//////////////////////////////////////////////////////
+
+		///// Root /////
+		this.addComponents(substitute, horizontal);
+		this.setMargin(true);
+		this.setSpacing(true);
 
 	}
 
