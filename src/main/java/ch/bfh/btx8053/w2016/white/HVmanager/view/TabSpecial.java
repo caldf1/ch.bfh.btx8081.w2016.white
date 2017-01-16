bbbac593f44e2b6c9858269574d6b35a4854779b
nallm1 <mamtha.sivagulanathan@students.bfh.ch>
2017-01-16 18:40:27 +0100

    Klasse TabSpecial angepasst

--- src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/TabSpecial.java
+++ src/main/java/ch/bfh/btx8053/w2016/white/HVmanager/view/TabSpecial.java
@@ -2,7 +2,10 @@ package ch.bfh.btx8053.w2016.white.HVmanager.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
@@ -11,7 +14,7 @@ import com.vaadin.ui.VerticalLayout;
 
 /**
  * 
- * @author degeg1
+ * @author degeg1, nallm1
  * @version 0.1
  */
 public class TabSpecial extends VerticalLayout implements View {
@@ -19,17 +22,28 @@ public class TabSpecial extends VerticalLayout implements View {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 681935335878275817L;
 	private VerticalLayout cID;
 	private VerticalLayout cName;
 	private VerticalLayout cFirstName;
 	private HorizontalLayout clientDetails;
+	private HorizontalLayout fearFieldBtns;
+	private HorizontalLayout aggressFieldBtns;
+	private HorizontalLayout dangerFieldBtns;
+	private Button addBtn1;
+	private Button cancelBtn1;
+	private Button editBtn1;
+	private Button addBtn2;
+	private Button cancelBtn2;
+	private Button editBtn2;
+	private Button addBtn3;
+	private Button cancelBtn3;
+	private Button editBtn3;
+	private Grid fear;
+	private Grid aggress;
+	private Grid danger;
 	private Label clientId;
 	private Label clientLastname;
 	private Label clientFirstname;
-	private ThemeResource resource;
-	private Image image;
-	
 	/**
 	 * 
 	 */
@@ -62,11 +76,60 @@ public class TabSpecial extends VerticalLayout implements View {
 		clientDetails.addComponents(cID, cName, cFirstName);
 		//////////////////////////////////////////////////////
 
-		// Image
-		this.resource = new ThemeResource("SiteUnderConstruction.jpeg");
-		this.image = new Image("", resource);
+		///// Fear Grid /////
+		this.fear = new Grid("Ängste:");
+		fear.setSizeFull();
+		fear.addColumn("Definition");
+		fear.addRow("Platzangst");
+		fear.addRow("Höhenangst");
 
-		this.addComponents(clientDetails, image);
+		this.fearFieldBtns = new HorizontalLayout();
+		fearFieldBtns.setSpacing(true);
+
+		this.addBtn1 = new Button(FontAwesome.PLUS);
+		this.cancelBtn1 = new Button(FontAwesome.MINUS);
+		this.editBtn1 = new Button(FontAwesome.EDIT);
+
+		fearFieldBtns.addComponents(addBtn1, cancelBtn1, editBtn1);
+		fearFieldBtns.setSpacing(true);
+		//////////////////////////////////////////////////////
+
+		///// Aggression Grid /////
+		this.aggress = new Grid("Aggression:");
+		aggress.setSizeFull();
+		aggress.addColumn("Definition");
+		aggress.addRow("Kann bei persönlichen Themen sehr gereizt reagieren.");
+
+		this.aggressFieldBtns = new HorizontalLayout();
+		aggressFieldBtns.setSpacing(true);
+
+		this.addBtn2 = new Button(FontAwesome.PLUS);
+		this.cancelBtn2 = new Button(FontAwesome.MINUS);
+		this.editBtn2 = new Button(FontAwesome.EDIT);
+
+		aggressFieldBtns.addComponents(addBtn2, cancelBtn2, editBtn2);
+		//////////////////////////////////////////////////////
+
+		///// Danger Grid /////
+		this.danger = new Grid("Selbst-/Fremdgefahr:");
+		danger.setSizeFull();
+		danger.addColumn("Definition");
+		danger.addRow("Bei zu nahe Treten Vorsicht geboten");
+
+		this.dangerFieldBtns = new HorizontalLayout();
+		dangerFieldBtns.setSpacing(true);
+
+		this.addBtn3 = new Button(FontAwesome.PLUS);
+		this.cancelBtn3 = new Button(FontAwesome.MINUS);
+		this.editBtn3 = new Button(FontAwesome.EDIT);
+
+		dangerFieldBtns.addComponents(addBtn3, cancelBtn3, editBtn3);
+		//////////////////////////////////////////////////////
+
+		///// Root /////
+		this.addComponents(clientDetails, fear, fearFieldBtns, aggress, aggressFieldBtns, danger, dangerFieldBtns);
+		this.setSpacing(true);
+
 	}
 
 }
